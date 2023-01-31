8c9bb19b517ae778b8316f74a6de0c69da3499b4
suncream100 <murmuring91@gmail.com>
2023-01-31 23:36:46 +0900

    add: chapter3 - TypeScript (doing)

---
+++ react-master-class/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="black" bgColor="teal" />
+      <Circle bgColor="tomato" text="Hello!" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-master-class/src/Circle.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: ${(props) => props.bgColor};
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+}
+
+export default Circle;
